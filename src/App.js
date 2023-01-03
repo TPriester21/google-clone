039421c691902d791598c6ed94dc8db52bf3496d
Tyler Priester <tylerpriester21@Tylers-MacBook-Pro.local>
2023-01-03 18:39:56 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Home from './components/Home';
-function App(){
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SearchPage from './components/SearchPage';
+
+export default function App() {
+
   return (
-    <div className='App'>
-      <Home />
-      <h1>Dr. Vicki's Google Clone</h1>
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}>
+
+          </Route>
+          <Route path="/search" element={<SearchPage/>}>
+
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
-}
-
-export default App;
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { SearchProvider } from './SearchProvider';
+import reducer, { initialState } from './reducer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
+  <SearchProvider initialState={initialState} reducer={reducer}>
     <App />
-  </React.StrictMode>
-);
 
+  </SearchProvider>
+);
